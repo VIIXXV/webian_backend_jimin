137bb19dfcd2d7921ce7ad780a75cd19b15a1a0d
Shin Yejin <yejin0605@sookmyung.ac.kr>
2024-01-25 15:40:39 +0900

    setting

---
+++ uhang/src/main/java/com/example/uhang/UhangApplication.java
@@ -0,0 +1,13 @@
+package com.example.uhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UhangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UhangApplication.class, args);
+	}
+
+}
